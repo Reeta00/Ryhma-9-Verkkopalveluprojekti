dcee88dde323ec34ea3baa5c40883d94d950852b
jkkafo <127877168+jkkafo@users.noreply.github.com>
2023-11-23 14:32:35 +0200

    alkusivu

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+// Product component to display individual products
+const Product = ({ name, price, image }) => (
+  <div className="product">
+    <img src={image} alt={name} />
+    <h3>{name}</h3>
+    <p>${price}</p>
+    <button>Add to Cart</button>
+  </div>
+);
+
+// ProductList component to display a list of products
+const ProductList = () => {
+  // Sample product data
+  const products = [
+    {
+      id: 1,
+      name: 'Kirja 1',
+      price: 10,
+      image: 'https://via.placeholder.com/150',
+    },
+    {
+      id: 2,
+      name: 'Kirja 2',
+      price: 15,
+      image: 'https://via.placeholder.com/150',
+    },
+    // Add more product objects as needed
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="product-list">
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          name={product.name}
+          price={product.price}
+          image={product.image}
+        />
+      ))}
     </div>
   );
-}
+};
+
+// Main App component
+const App = () => {
+  return (
+    <div className="app">
+      <h1>Tervetuloa novellinurkkaan!</h1>
+      <ProductList />
+    </div>
+  );
+};
 
 export default App;
